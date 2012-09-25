6105fb581963df462502f29268a2fd06715ead67
Shawn Tan <shawn@wtf.sg>
2012-09-26 05:20:02 +0800

    FileWrapper written

--- lib/io/writer.py
+++ lib/io/writer.py
@@ -6,9 +6,30 @@ Created on Sep 24, 2012
 from lib.options.config import configuration as config
 import __builtin__
 
+class FileWrapper(object):
+	def __init__(self,obj):
+		self.obj = obj
+	
+	def close(self,*args,**kwargs):
+		print "Closing file..."
+		self.obj.close(*args,**kwargs)
+
+	def __defattr__(self,attr):
+		if hasattr(self.obj, attr):
+			attr_value = getattr(self.obj,attr)
+			if isinstance(attr_value,types.MethodType):
+				def callable(*args, **kwargs):
+					return attr_value(*args, **kwargs)
+				return callable
+			else:
+				return attr_value
+		else:
+			raise AttributeError
+
+
 def marked_open(*params):
 	global _open
-	print "Opening file with marked_open", params[1]
+	print "Opening file..."
 	print config.filename_formats.date
 	return _open(*params)
 
